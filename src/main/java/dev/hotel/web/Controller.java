c8e7b2a3ac9d3d7856b538e6f6de761854564c65
sylvain amianto <samianto@gmail.com>
2020-09-17 09:10:18 +0200

    migration vers gitHub

---
+++ src/main/java/dev/hotel/web/Controller.java
@@ -0,0 +1,44 @@
+package dev.hotel.web;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.hotel.entite.Client;
+import dev.hotel.repository.ClientRepository;
+
+@RestController
+public class Controller {
+
+	private ClientRepository clientRepository;
+
+	public Controller(ClientRepository clientRepository) {
+		super();
+		this.clientRepository = clientRepository;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "clients")
+	public List<Client> listClient(@RequestParam int start, @RequestParam int size) {
+		return clientRepository.findAll(PageRequest.of(start, size)).getContent();
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "clients/{UUID}")
+	public ResponseEntity<?> getClientUUID(@PathVariable UUID UUID) {
+		if (clientRepository.findById(UUID).isPresent()) {
+			return ResponseEntity.status(HttpStatus.OK).header("message", "cool").body(clientRepository.findById(UUID));
+		} else {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Veuillez entrer un autre identifiant client");
+		}
+
+	}
+
+}
